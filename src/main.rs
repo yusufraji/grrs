e2ced1a5670535dfc6027e6d552bd84b22c84240
Yusuf Raji <raji.yusuf234@gmail.com>
2022-07-14 23:36:36 +0800

    Handle errors elegantly

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #![allow(unused)]
 use clap::Parser;
+use anyhow::{Context, Result};
 
 // Search for a pattern in& a file and display the lines that contain it.
 #[derive(Parser)]
@@ -10,15 +11,16 @@ struct Cli {
     #[clap(parse(from_os_str))]
     path: std::path::PathBuf,
 }
-fn main() {
+fn main() -> Result<()> {
     let args = Cli::parse();
     println!("pattern to search for: {}", args.pattern);
     println!("file path to read: {}", args.path.display());
 
-    let content = std::fs::read_to_string(&args.path).expect("could not read file");
+    let content = std::fs::read_to_string(&args.path).with_context(|| format!("could not read file '{}'", args.path.display()))?;
     for line in content.lines() {
         if line.contains(&args.pattern) {
             println!("{}", line);
         }
     }
+    Ok(())
 }
