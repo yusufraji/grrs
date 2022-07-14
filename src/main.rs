416c2acad55104270e8d8feb6c29f4b3524b4dbf
Yusuf Raji <raji.yusuf234@gmail.com>
2022-07-14 22:54:31 +0800

    Print lines with pattern arg

--- src/main.rs
+++ src/main.rs
@@ -14,4 +14,11 @@ fn main() {
     let args = Cli::parse();
     println!("pattern to search for: {}", args.pattern);
     println!("file path to read: {}", args.path.display());
+
+    let content = std::fs::read_to_string(&args.path).expect("could not read file");
+    for line in content.lines() {
+        if line.contains(&args.pattern) {
+            println!("{}", line);
+        }
+    }
 }
