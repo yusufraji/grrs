d30b72afaace0fbd407901efca85babdbd914567
Yusuf Raji <raji.yusuf234@gmail.com>
2022-07-15 16:15:18 +0800

    Add integration test, find_content_in_file

--- tests/cli.rs
+++ tests/cli.rs
@@ -1,4 +1,5 @@
 use assert_cmd::prelude::*; // Add methods on commands
+use assert_fs::prelude::*;
 use predicates::prelude::*; // Used for writing assertions
 use std::process::Command; // Run programs
 
@@ -10,6 +11,20 @@ fn file_doesnt_exist() -> Result<(), Box<dyn std::error::Error>> {
     cmd.assert()
         .failure()
         .stderr(predicate::str::contains("could not read file"));
+
+    Ok(())
+}
+
+#[test]
+fn find_content_in_file() -> Result<(), Box<dyn std::error::Error>> {
+    let file = assert_fs::NamedTempFile::new("sample.txt")?;
+    file.write_str("A test\nActual content\nMore content\nAnother test")?;
+
+    let mut cmd = Command::cargo_bin("grrs")?;
+    cmd.arg("test").arg(file.path());
+    cmd.assert()
+        .success()
+        .stdout(predicate::str::contains("test\nAnother test"));
 
     Ok(())
 }
